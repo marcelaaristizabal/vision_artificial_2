236446f7cea7c9a1c732e4eda782ad45e55c2bbc
marcelaaristizabal <aristizabal.marcela@uces.edu.co>
2023-02-14 22:21:28 -0500

    Clase 3

--- Clase 3/ejercicio1.py
+++ Clase 3/ejercicio1.py
@@ -1,22 +1,36 @@
-"""
-Punto de partida
-"""
-
 import numpy as np
+# import matplotlib.pyplot as plt
+import math
 
-LR = 0.00005
-EPOCHS = 20
+LR = 0.0003
+EPOCHS = 10
 
-dataset = np.loadtxt('train.csv', delimiter=";")
-X = dataset[:,0,None] # Preservar la dimensión y que quede como un vector
-y = dataset[:,1,None]
+dt = np.loadtxt('train.csv', delimiter=";")
+x = dt[:,0, None]
+y = dt[:,1, None]
 
-# Estandarizar datos
+# Estandarizar
+x = (x-np.mean(x))/np.std(x)
 
-# Agregar columna de 1 a X para multiplicar por theta_0
-m = X.shape[0]
-X_0 = np.ones((m,1))
-X = np.hstack((X_0,X))
+# Columna de 1 a X
+m = x.shape[0]
+x_0 = np.ones((m,1))
+x = np.hstack((x_0,x))
 
-# Parámetros a optimizar
+# Primeros valores del parametro
 theta = np.random.rand(2)
+for i in range(EPOCHS):
+    
+    # Predecidos
+    y_pred = x * theta
+    err = y_pred - y
+    errx= (y_pred-y) * x[:,1,None]
+    
+    # Derivadas parciales
+    dpar0 = np.sum(err)
+    dpar1 = np.sum(errx)
+    
+    theta[0] -= LR * dpar0
+    theta[1] -= LR * dpar1
+
+print(theta[0],theta[1])
